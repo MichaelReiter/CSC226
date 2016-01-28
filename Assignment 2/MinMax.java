4d0f8af2c4a1a11714e5c8da9330c7ad8bf25704
Michael Reiter <michaelreiter@live.ca>
2016-01-28 10:42:50 -0800

    Add Assignment 2 starter files

---
+++ Assignment 2/MinMax.java
@@ -0,0 +1,18 @@
+public class MinMax {
+
+  // Ususal divide and conquer n/2, n/2 split
+  public static Pair mmA(int lb, int ub, int[] a) {
+
+    return null;
+  }
+
+  // Refined split to always use ceil(3n/2 - 2) comparisons
+  public static Pair mmB(int lb, int ub, int[] a) {
+
+    return null;
+  }
+
+  public static void main(String[] args) {
+    
+  }
+}
---
+++ Assignment 2/Pair.java
@@ -0,0 +1,9 @@
+public class Pair {
+  int alpha;    // the smaller one
+  int omega;    // the bigger one
+
+  Pair(int a, int o) {
+    alpha = a;
+    omega = o;
+  }
+}
