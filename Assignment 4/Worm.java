8ec9077ede5f0deab4f3bb38a23ef98664a4b059
MichaelReiter <michaelreiter@live.ca>
2016-03-10 19:18:00 -0800

    Add temp returns and dependencies so program compiles

--- Assignment 4/Worm.java
+++ Assignment 4/Worm.java
@@ -2,20 +2,23 @@ public class Worm {
 
   // PUT YOUR CLASS VARIABLE HERE
 
-  public Worm( In in ) { 
+  public Worm(In in) { 
     // Create a new problem instance.
   }
 
-  public double dist( String origP, String destP ) {
+  public double dist(String origP, String destP) {
     // return the distance from origP to destP
+    return 1; //temp
   }
 
-  public int   worms( String origP, String destP ) { 
+  public int worms(String origP, String destP) { 
     // least number of wormholes in any shortest path from origP to destP
+    return 1; //temp
   }
 
-  public String query( String origP, String destP ) {
+  public String query(String origP, String destP) {
     // Output the "The distance from ... wormholes." string.
+    return "temp";
   }
 
   public static void main(String[] args) {
@@ -26,7 +29,7 @@ public class Worm {
       System.out.println("Case " + t + ":");
       Worm w = new Worm(in);
       int Q = in.readInt();
-      for (int i=0; i<Q; i++) {
+      for (int i = 0; i < Q; i++) {
         String p1s = in.readString();
         String p2s = in.readString();
         System.out.println(w.query(p1s, p2s));
